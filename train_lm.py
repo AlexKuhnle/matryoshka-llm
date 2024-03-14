ae60afef9b2741ff311d485504e6aa48c8c5fe31
AlexKuhnle <alexkuhnle@t-online.de>
2024-03-14 13:19:35 +0000

    Add data_fraction

--- train_lm.py
+++ train_lm.py
@@ -12,7 +12,6 @@ from modules.rms_norm import RMSNorm
 
 
 if __name__ == "__main__":
-    DEBUG = False
     dataset_name = sys.argv[1]
     model_name = sys.argv[2]
     suffix = sys.argv[3]
@@ -28,13 +27,6 @@ if __name__ == "__main__":
     dataset = datasets.load_dataset(dataset)
     train_dataset = dataset["train"]
     test_dataset = dataset["validation"]
-
-    if num_epochs < 1.0:
-        train_dataset = train_dataset.select(list(range(int(len(train_dataset) * num_epochs))))
-        num_epochs = 1
-    elif DEBUG:
-        train_dataset = train_dataset.select(list(range(10)))
-        test_dataset = test_dataset.select(list(range(10)))
 
     tokenizer = tokenizers.Tokenizer.from_file(f"data/tokenizers/{dataset_name}-bpe")
     os.makedirs(f"lightning_logs_lm/{dataset_name}-{model_name}-{suffix}", exist_ok=True)
@@ -80,7 +72,8 @@ if __name__ == "__main__":
             gradient_clipping=1.0,
             accumulate_grad_batches=1,
             precision="16",
-        )
+            data_fraction=1.0,
+        ),
     )
     model.cuda()
 
@@ -101,6 +94,11 @@ if __name__ == "__main__":
 
         train_dataset = split_tinyshakespeare(train_dataset)
         test_dataset = split_tinyshakespeare(test_dataset)
+
+    if model.trainer_kwargs["data_fraction"] < 1.0:
+        train_dataset = train_dataset.select(
+            list(range(int(len(train_dataset) * model.trainer_kwargs["data_fraction"])))
+        )
 
     print(f"train: {len(train_dataset)}")
     print(f"test:  {len(test_dataset)}")
